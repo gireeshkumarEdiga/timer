93d74049425b35b96ed84923ce64a975e4a0543f
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2021-12-31 22:27:57 +0530

    code submitted

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Timer } from "./components/Timer";
+import { useState } from "react";
+
+function App() {
+  const [ show, setShow] = useState(false);
+  return (
+    <div className="App">
+      <h1 style={{color:"blue"}}>Timer</h1>
+      {show ? <Timer />: null}
+      <button style={{background:"green", border : "green", width:"70px", height:"30px"}} onClick={() => {setShow(!show)}}> { show ? "hide" : "show"}</button>
+    </div>
+  );
+}
+
+export default App;
